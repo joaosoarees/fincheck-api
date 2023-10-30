1a36cd653f97289ac0cde8b8c69176b99cf1ff79
Joao Soares <jsoareez@gmail.com>
2023-10-29 22:37:32 -0300

    feat: add global auth guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -9,6 +9,7 @@ import { env } from 'src/shared/config/env';
 @Module({
   imports: [
     JwtModule.register({
+      global: true,
       secret: env.jwtSecret,
       signOptions: { expiresIn: '7d' },
     }),
